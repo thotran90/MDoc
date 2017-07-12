2816ccd10fa216dc084a009348b68ee9e60c5351
Trần Công Thọ <trancongtho@outlook.com>
2017-07-13 01:50:52 +0700

    add shoole service

---
+++ MDoc/MDoc.Services.Contract/DataContracts/SchoolModel.cs
@@ -0,0 +1,19 @@
+﻿namespace MDoc.Services.Contract.DataContracts
+{
+    public class SchoolModel
+    {
+        public int SchoolId { get; set; }
+        public byte SchoolTypeId { get; set; }
+        public string Name { get; set; }
+        public string Website { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public int CountryId { get; set; }
+        public int ProvinceId { get; set; }
+        public int DistrictId { get; set; }
+        public int WardId { get; set; }
+        public int LoggedUserId { get; set; }
+    }
+}
---
+++ MDoc/MDoc.Services.Contract/Interfaces/ISchoolService.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using MDoc.Services.Contract.DataContracts;
+
+namespace MDoc.Services.Contract.Interfaces
+{
+    public interface ISchoolService
+    {
+        IQueryable<SchoolModel> GetSchools(string query);
+        SchoolModel Create(SchoolModel model);
+        SchoolModel Update(SchoolModel model);
+        bool Remove(SchoolModel model);
+    }
+}
