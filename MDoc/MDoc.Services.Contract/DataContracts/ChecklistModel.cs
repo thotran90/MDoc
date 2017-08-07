0d5e8bbb7e10cbcc6cb8b19e8c003cc314fae781
Trần Công Thọ <trancongtho@outlook.com>
2017-08-08 02:08:40 +0700

    start work with checklist

---
+++ MDoc/MDoc.Services.Contract/DataContracts/ChecklistModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MDoc.Services.Contract.DataContracts
+{
+    public class ChecklistModel
+    {
+        public byte Id { get; set; }
+        public string Label { get; set; }
+        public string Description { get; set; }
+        public int LoggedUserId { get; set; }
+    }
+}
---
+++ MDoc/MDoc.Services.Contract/Interfaces/IChecklistService.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using MDoc.Services.Contract.DataContracts;
+
+namespace MDoc.Services.Contract.Interfaces
+{
+    public interface IChecklistService
+    {
+        IQueryable<ChecklistModel> ListOfItems();
+    }
+}
